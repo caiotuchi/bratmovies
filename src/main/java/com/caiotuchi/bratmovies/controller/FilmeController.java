ad67528931cff8f23c4709a2d2245fa796bb0bc4
Caio Alves <caiogt88@hotmail.com>
2024-10-20 18:17:12 -0300

    Implementa método Post para poder adicionar filmes

--- src/main/java/com/caiotuchi/bratmovies/controller/FilmeController.java
+++ src/main/java/com/caiotuchi/bratmovies/controller/FilmeController.java
@@ -3,9 +3,7 @@ package com.caiotuchi.bratmovies.controller;
 import com.caiotuchi.bratmovies.model.Filme;
 import com.caiotuchi.bratmovies.service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +18,10 @@ public class FilmeController {
     public List<Filme> getFilmes() {
         return filmeService.listarFilmes();
     }
+
+    @PostMapping
+    public Filme adicionarFilme(@RequestBody Filme filme) {
+        return filmeService.salvarFilme(filme);
+    }
+
 }
